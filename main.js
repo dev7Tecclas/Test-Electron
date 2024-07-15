55deb9d45eb590e134687a7966ffe1edeaa8c94a
dev7Tecclas <122485391+dev7Tecclas@users.noreply.github.com>
2024-07-15 11:10:52 -0500

    test

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // preload: path.join(__dirname, "preload.js"),
+
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Establecer el User Agent a uno compatible
+  win.webContents.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+  );
+
+  console.log("hola papu");
+
+  win.loadURL("https://web.whatsapp.com");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
